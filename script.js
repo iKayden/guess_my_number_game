4d6e3230d661bc3e58ceae1e1f0cc6e58c2bf49d
ikayden <ikyden23@gmail.com>
2022-12-13 11:37:08 -0800

    add selectors

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const message = document.querySelector(".message");
+const number = document.querySelector(".number");
+const score = document.querySelector(".score");
+const guessValue = document.querySelector(".guess").value;
+const checkBtn = document.querySelector(".check");
+
+checkBtn.addEventListener("click", function() {
+  console.log(document.querySelector(".guess").value);
+});
