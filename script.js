feb398ee0fcfdb6af8b7904bb78fbe6f20e54428
ikayden <ikyden23@gmail.com>
2022-12-15 15:06:54 -0800

    add more logic and css change

--- script.js
+++ script.js
@@ -7,19 +7,31 @@ const score = document.querySelector(".score");
 const checkBtn = document.querySelector(".check");
 const randNumber = Math.trunc(Math.random() * 20) + 1;
 number.textContent = randNumber;
+let setScore = 20;
+
 
 checkBtn.addEventListener("click", function() {
   const guessValue = Number(document.querySelector(".guess").value);
+
   if (!guessValue) {
     return message.textContent = "❌ No number!";
   }
+  if (!setScore) {
+    return message.textContent = "💥💥💥You've lost! Try again!";
+  }
+
   if (guessValue === randNumber) {
     message.textContent = "You guessed right! 🎉🎉🎉";
+    document.querySelector('body').style.backgroundColor = "#60b347";
+    number.style.width = "30rem";
+    number.style.backgroundColor = "gold";
   } else if (guessValue > randNumber) {
     message.textContent = "Your guess is too high!";
-    score.textContent -= 1;
+    setScore--;
+    score.textContent = setScore;
   } else if (guessValue < randNumber) {
     message.textContent = "Your guess is too low!";
-    score.textContent -= 1;
+    setScore--;
+    score.textContent = setScore;
   }
 });
