9b26a16be77f8bd06064f5b868e89373daf1f9e5
ikayden <ikyden23@gmail.com>
2022-12-15 14:12:12 -0800

    add rand number, logic and score count.

--- script.js
+++ script.js
@@ -3,9 +3,23 @@
 const message = document.querySelector(".message");
 const number = document.querySelector(".number");
 const score = document.querySelector(".score");
-const guessValue = document.querySelector(".guess").value;
+
 const checkBtn = document.querySelector(".check");
+const randNumber = Math.trunc(Math.random() * 20) + 1;
+number.textContent = randNumber;
 
 checkBtn.addEventListener("click", function() {
-  console.log(document.querySelector(".guess").value);
+  const guessValue = Number(document.querySelector(".guess").value);
+  if (!guessValue) {
+    return message.textContent = "❌ No number!";
+  }
+  if (guessValue === randNumber) {
+    message.textContent = "You guessed right! 🎉🎉🎉";
+  } else if (guessValue > randNumber) {
+    message.textContent = "Your guess is too high!";
+    score.textContent -= 1;
+  } else if (guessValue < randNumber) {
+    message.textContent = "Your guess is too low!";
+    score.textContent -= 1;
+  }
 });
